49321063a16ae6505107d1ecd14ce0b759ace185
Techwinx1 <einnocent99@gmail.com>
2020-02-20 00:10:13 +0100

    update the time display to show am or pm

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const $ = (n) => document.querySelector(n);
 const hours = $('#hours');
 const minutes = $('#minutes');
 const seconds = $('#seconds');
+const am_pm = $('#am_pm');
 
 function updateTime(){
     const date = new Date();
     hours.textContent = date.getHours();
     minutes.textContent= date.getMinutes();
     seconds.textContent= date.getSeconds();
+    if(hours/12 >= 1){
+        am_pm.textContent= "PM";
+    }else{
+        am_pm.textContent="AM";
+    }
 }
 
 setInterval(updateTime, 1000)
